e20e769d470c15191edeeb90b4d897650488538f
Hriday-S <67763187+Hriday-S@users.noreply.github.com>
2023-04-26 10:59:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import cv2
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect()
+
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+cv2.imwrite("djifile.png", frame_read.frame)
 
