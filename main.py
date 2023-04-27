c463b5328d768392fb055760f356d62632e4ef25
Hriday-S <67763187+Hriday-S@users.noreply.github.com>
2023-04-27 11:11:58 -0400

    Update main.py
    video instead of pictures

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import cv2
 from djitellopy import Tello
+import threading
 
+# Initialize Tello drone
 tello = Tello()
 tello.connect()
 
+
 tello.streamon()
-frame_read = tello.get_frame_read()
 
-cv2.imwrite("djifile.png", frame_read.frame)
+# Get video stream
+video_stream = tello.get_frame_read()
 
+tello.takeoff()
+# Read and display video frames
+while True:
+    # Get the latest video frame
+    frame = video_stream.frame
+
+    # Display the video frame
+    cv2.imshow("Tello Video Stream", frame)
+    cv2.waitKey(1)
