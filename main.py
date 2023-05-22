9e5e143349646dc1775b04a2a500074f9c676ec8
Hriday-S <67763187+Hriday-S@users.noreply.github.com>
2023-05-21 21:08:16 -0400

    Update main.py
    added tkinter GUI for tele-op control

--- main.py
+++ main.py
@@ -2,45 +2,85 @@ import cv2
 import numpy as np
 from djitellopy import Tello
 import datetime
+import tkinter as tk
+from tkinter import font
+import threading
+from PIL import Image, ImageTk
 
+# Initialize Tello drone
 tello = Tello()
 tello.connect()
-
 tello.streamon()
-frame_read = tello.get_frame_read()
-
- 
-def getColor(image):
-    average_color_row = np.average(image, axis=0)
-    average_color = np.average(average_color_row, axis=0)
-    return average_color
-def getGreenDifferential(rgb):
-    return (abs(rgb[0]/rgb[1]) + abs(rgb[2]/rgb[1]))/2
 
 
-image = frame_read.frame
-
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-# Define lower and upper range of green color in HSV
-lower_green = np.array([40, 100, 70])
-upper_green = np.array([75, 255, 255])
-# Create a mask of pixels that fall within the range of the green color
-green_mask = cv2.inRange(hsv, lower_green, upper_green)
-# The mask is applied on the original image
-green_image = cv2.bitwise_and(image, image, mask=green_mask)
-   
-# Save the resulting image
-cv2.imwrite("Filtration{}.png".format(datetime.datetime.now()), green_image)
 
 
+window = tk.Tk()
+
+button_font = font.Font(size = 16)
+
+scale = 100
+forward = tk.Button(window, text = "FOWARD", width = 10, height = 5, font = button_font, command = lambda:tello.move_forward(scale))
+forward.grid(row = 0, column = 2)
+
+backward = tk.Button(window, text = "BACKWARD", width = 10, height = 5, font = button_font, command = lambda:tello.move_back(scale))
+backward.grid(row = 2, column = 2)
+
+left = tk.Button(window, text = "LEFT", width = 10, height = 5, font = button_font, command = lambda:tello.move_left(scale))
+left.grid(row = 1, column = 1)
+
+takeOff = tk.Button(window, text = "Take Off", width = 10, height = 5, font = button_font, command = tello.takeoff)
+takeOff.grid(row = 1, column = 5)
+
+land = tk.Button(window, text = "Land", width = 10, height = 5, font = button_font, command = tello.land)
+land.grid(row = 2, column = 5)
+
+right = tk.Button(window, text = "RIGHT", width = 10, height = 5, font = button_font, command = lambda:tello.move_right(scale))
+right.grid(row = 1, column = 4)
+
+up = tk.Button(window, text = "UP", width = 10, height = 5, font = button_font, command = lambda:tello.move_up(scale))
+up.grid(row =0, column = 5)
+
+down = tk.Button(window, text = "DOWN", width = 10, height = 5, font = button_font, command = lambda:tello.move_down(scale))
+down.grid(row =3, column = 5)
+
+battery = tk.Label(window, text = "Battery: {}%".format(tello.get_battery()))
+battery.grid(row = 4, column = 5)
+
+canvas = tk.Canvas(window, width = 960/2, height = 720/2)
+canvas.grid(row = 5, column = 0)
+
+def update_video():
+    while True:
+        frame = cv2.resize(tello.get_frame_read().frame, (580, 360))
+        framehsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        hue_channel = framehsv[:,:, 0]
+        saturation_channel = framehsv[:,:, 1]
+        value_channel = framehsv[:,:, 2]
+
+        hue_min = 60
+        hue_max = 110
+
+
+        mask = (hue_channel < hue_max) & (hue_channel > hue_min)
         
-average_color = getColor(frame_read.frame)
-greenDifferential = getGreenDifferential(average_color)
-print("Green differntial: " + str(greenDifferential))
+        filtered_image = np.zeros_like(frame)
+        filtered_image[mask] = frame[mask]
+        
+        image = Image.fromarray(filtered_image)
+        photo = ImageTk.PhotoImage(image)
+        canvas.create_image(0,0, anchor = tk.NW, image = photo)
+        canvas.image = photo
+        
+video_thread = threading.Thread(target=update_video)
+video_thread.daemon = True
 
 
+video_thread.start()
 
 
+window.mainloop()
 
-
-
+telllo.streamoff()
+tello.end()
